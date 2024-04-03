1ee5591f471763458afb930a8434509d9ec7bf08
Mwungere <mwungereelite56@gmail.com>
2024-04-03 16:46:12 +0200

    about

--- src/pages/blog/blog-dark.jsx
+++ src/pages/blog/blog-dark.jsx
@@ -35,7 +35,7 @@ const BlogDark = () => {
       </div>
       <Navbar nr={navbarRef} lr={logoRef} />
       <PageHeader
-        title="News."
+        title="Our News."
         paragraph="All the most current news and events of our creative team."
       />
       <BlogStanderd blogs={blog1Data} />
